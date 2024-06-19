1a923e6d7c1f30d7647ae34d5c33af3e4f923d65
Joshua Pickard <jpic@umich.edu>
2024-06-19 17:17:58 -0400

    Update build.py

--- HWG/build.py
+++ HWG/build.py
@@ -1,3 +1,6 @@
+import os
+import urllib.request
+
 ### CODES TO DOWNLOAD DATA ###
 def download_data():
     """This function should call the methods to download the STRING and HURI datasets.
@@ -15,7 +18,26 @@ def download_huri():
     """This function should download the HuRI database to the computer.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 18-26
     """
-    pass
+    # Define the path for downloaded data
+    downloaded_data_path = "path/to/your/downloaded/data/"
+    HuRI_path = os.path.join(downloaded_data_path, "HuRI")
+    
+    # Create the directory if it doesn't exist
+    os.makedirs(HuRI_path, exist_ok=True)
+    
+    # Define file paths
+    HuRI_file = os.path.join(HuRI_path, "HuRI.tsv")
+    HI_union_file = os.path.join(HuRI_path, "HI-union.tsv")
+    
+    # Download the files if they don't exist
+    if not os.path.isfile(HuRI_file):
+        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HuRI.tsv", HuRI_file)
+    
+    if not os.path.isfile(HI_union_file):
+        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HI-union.tsv", HI_union_file)
+    
+    print("HuRI Downloaded")
+
 
 def download_HGNC():
     """This function should download the HGNC database to the computer.
