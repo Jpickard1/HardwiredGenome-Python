330d17a22140d2c2c7090510a1f0ec87f1e980bd
Joshua Pickard <jpic@umich.edu>
2024-06-19 16:40:59 -0400

    Create build.py

---
+++ HWG/build.py
@@ -0,0 +1,43 @@
+### CODES TO DOWNLOAD DATA ###
+def download_data():
+    """This function should call the methods to download the STRING and HURI datasets.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m
+    """
+    pass
+
+def download_string():
+    """This function should download the STRING database to the computer.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 28-34
+    """
+    pass
+
+def download_huri():
+    """This function should download the HuRI database to the computer.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 18-26
+    """
+    pass
+
+def download_HGNC():
+    """This function should download the HGNC database to the computer.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 46-54
+    """
+    pass
+
+def download_HumanTF():
+    """This function should download the Human Transcription Factors database to the computer.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 56-64
+    """
+    pass
+
+### CODES TO BUILD INDEX TABLE ###
+def buildIndexTable():
+    """This function should build an index table to map each entry of the A matrix to gene names.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/buildIndexTable.m
+    """
+    pass
+
+### CODES TO BUILD HARDWIRED GENOME A MATRIX ###
+def buildAdjacencyMatrix():
+    """This function assembles the STRING and HuRI downloaded data into an adjacency matrix.
+    See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/buildHWGobj.m
+    """
