4d73b89c05016fe6323bf286f0d5d1009072fbe8
Yuchen Shao <ycshao@umich.edu>
2024-06-23 18:32:18 -0400

    all download function finished and working correctly, test by using diff in command lines, start building functions

--- HWG/.ipynb_checkpoints/build-checkpoint.py
+++ HWG/.ipynb_checkpoints/build-checkpoint.py
@@ -25,44 +25,61 @@ def download_data():
     os.makedirs(STRING_path, exist_ok=True)
 
     # HuRI data
-    HuRI_tsv = os.path.join(HuRI_path, "HuRI.tsv")
-    HI_union_tsv = os.path.join(HuRI_path, "HI-union.tsv")
+    HuRI_file = os.path.join(HuRI_path, "HuRI.tsv")
+    HI_union_file = os.path.join(HuRI_path, "HI-union.tsv")
 
-    if not os.path.isfile(HuRI_tsv):
-        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HuRI.tsv", HuRI_tsv)
+    if not os.path.isfile(HuRI_file):
+        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HuRI.tsv", HuRI_file)
 
-    if not os.path.isfile(HI_union_tsv):
-        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HI-union.tsv", HI_union_tsv)
+    if not os.path.isfile(HI_union_file):
+        urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HI-union.tsv", HI_union_file)
 
     print("    HuRI Downloaded")
 
     # STRING data
-    STRING_txt_gz = os.path.join(STRING_path, "9606.protein.links.v11.5.txt.gz")
+    STRING_file = os.path.join(STRING_path, "9606.protein.links.v11.5.txt.gz")
     STRING_txt = os.path.join(STRING_path, "9606.protein.links.v11.5.txt")
 
-    if not os.path.isfile(STRING_txt_gz):
-        urllib.request.urlretrieve('https://stringdb-static.org/download/protein.links.v11.5/9606.protein.links.v11.5.txt.gz', STRING_txt_gz)
-        with gzip.open(STRING_txt_gz, 'rb') as f_in:
+    if not os.path.isfile(STRING_file):
+        urllib.request.urlretrieve('https://stringdb-static.org/download/protein.links.v11.5/9606.protein.links.v11.5.txt.gz', STRING_file)
+        with gzip.open(STRING_file, 'rb') as f_in:
             with open(STRING_txt, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
     print("    STRING Downloaded")
-    print("Current Working Directory:", os.getcwd())
+##print("Current Working Directory:", os.getcwd())
     pass
-download_data()
+
 ## after running download_data(), the data is downloaded outside of current local address.I mean, it is supposed to downloaded into D:/HardwiredGenome-Python/HWG,right? but now it is placed at D:/HardwiredGenome-(or it is what it is supposed to be, I am not familiar with how matlab code work, or it is just a problme caused by name?like how it deal with "-")
 def download_string():
     """This function should download the STRING database to the computer.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 28-34
     """
+    data_path = load_data_path()
+    downloaded_data_path = os.path.join(data_path, "Downloaded/")
+    STRING_path = os.path.join(downloaded_data_path, "STRING")
+    # Create the directory if it doesn't exist
+    os.makedirs(STRING_path, exist_ok=True)
+    # Define the file paths
+    STRING_file = os.path.join(STRING_path, "9606.protein.links.v11.5.txt.gz")
+    STRING_txt = os.path.join(STRING_path, "9606.protein.links.v11.5.txt")
+    if not os.path.isfile(STRING_file):
+        urllib.request.urlretrieve('https://stringdb-static.org/download/protein.links.v11.5/9606.protein.links.v11.5.txt.gz', STRING_file)
+        with gzip.open(STRING_file, 'rb') as f_in:
+            with open(STRING_txt, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    print("    STRING Downloaded")
     pass
+
 
 def download_huri():
     """This function should download the HuRI database to the computer.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 18-26
     """
     # Define the path for downloaded data
-    downloaded_data_path = "path/to/your/downloaded/data/"
+    data_path = load_data_path()
+    downloaded_data_path = os.path.join(data_path, "Downloaded/")
     HuRI_path = os.path.join(downloaded_data_path, "HuRI")
     
     # Create the directory if it doesn't exist
@@ -79,20 +96,44 @@ def download_huri():
     if not os.path.isfile(HI_union_file):
         urllib.request.urlretrieve("http://www.interactome-atlas.org/data/HI-union.tsv", HI_union_file)
     
-    print("HuRI Downloaded")
+    print("    HuRI Downloaded")
+    pass
 
 
 def download_HGNC():
     """This function should download the HGNC database to the computer.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 46-54
     """
+    data_path = load_data_path()
+    downloaded_data_path = os.path.join(data_path, "Downloaded/")
+    HGNC_path = os.path.join(downloaded_data_path, "HGNC")
+    # Create the directory if it doesn't exist
+    os.makedirs(HGNC_path, exist_ok=True)
+    # Define the file path
+    HGNC_file = os.path.join(HGNC_path, "gene_lookup_dictionary.tsv")
+    if not os.path.isfile(HGNC_file):
+        urllib.request.urlretrieve('http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt', HGNC_file)
+
+    print("    HGNC Downloaded")
     pass
+
 
 def download_HumanTF():
     """This function should download the Human Transcription Factors database to the computer.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/data_download.m lines 56-64
     """
+    data_path = load_data_path()
+    downloaded_data_path = os.path.join(data_path, "Downloaded/")
+    HuTF_path = os.path.join(downloaded_data_path, "HuTF")
+    # Create the directory if it doesn't exist
+    os.makedirs(HuTF_path, exist_ok=True)
+    HuTF_file = os.path.join(HuTF_path, "HuTF_db.csv")
+    if not os.path.isfile(HuTF_file):
+        urllib.request.urlretrieve('http://humantfs.ccbr.utoronto.ca/download/v_1.01/DatabaseExtract_v_1.01.csv', HuTF_file)
+
+    print("    HuTF Downloaded")
     pass
+
 
 ### CODES TO BUILD INDEX TABLE ###
 def buildIndexTable():
