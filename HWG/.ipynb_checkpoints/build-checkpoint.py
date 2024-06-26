8c54633c159766d0aa7a970c1aa12a4df5c2c46c
SYC25 <ycshao@umich.edu>
2024-06-26 15:40:50 -0400

    basically done, wait for correct test way

--- HWG/.ipynb_checkpoints/build-checkpoint.py
+++ HWG/.ipynb_checkpoints/build-checkpoint.py
@@ -2,6 +2,10 @@ import os
 import urllib.request
 import gzip
 import shutil
+from scipy.sparse import lil_matrix
+import pandas as pd
+import numpy as np
+import networkx as nx
 ### CODES TO DOWNLOAD DATA ###
 def load_data_path():
     filepath = os.path.dirname(os.path.abspath(__file__))
@@ -71,7 +75,7 @@ def download_string():
                 shutil.copyfileobj(f_in, f_out)
 
     print("    STRING Downloaded")
-
+download_string()
 
 def download_huri():
     """This function should download the HuRI database to the computer.
@@ -264,9 +268,169 @@ def buildIndexTable():
 ##extra function needed for buildAdjacencyMatrix()
 def list_HuRI():
     data_path = load_data_path()
+    HuRI_downloaded_data1 = f"{data_path}/Downloaded/HuRI/HuRI.tsv"
+    HuRI_downloaded_data2 = f"{data_path}/Downloaded/HuRI/Hi-union.tsv"
+
+    # Read the data
+    HuRI_PPI1 = pd.read_csv(HuRI_downloaded_data1, sep='\t')
+    HuRI_PPI2 = pd.read_csv(HuRI_downloaded_data2, sep='\t')
+
+    # Create the DataFrame
+    A_list = pd.DataFrame({
+        'Protein 1': HuRI_PPI1['Protein_1'].astype(str),
+        'Protein 2': HuRI_PPI2['Protein_2'].astype(str)
+    })
+
+    return A_list
+
+
+def list_STRING(thresh, STRING_PPI=None):
+    print("Accessing STRING data")
+
+    # Set data path
+    data_path = load_data_path()
+
+    # Read and sort raw PPIs
+    if STRING_PPI is None or not isinstance(STRING_PPI, pd.DataFrame):
+        print("    Reading data from file...")
+        STRING_downloaded_PPI = f"{data_path}/Downloaded/STRING/9606.protein.links.v11.5.txt"
+        STRING_PPI = pd.read_csv(STRING_downloaded_PPI, delim_whitespace=True)
+        print("    File read complete")
+    else:
+        print("    STRING data present from previous file read")
+
+    # Apply threshold
+    confident_indices = STRING_PPI['combined_score'].astype(int) > thresh
+    P1_confident = STRING_PPI['protein1'][confident_indices].astype(str)
+    P2_confident = STRING_PPI['protein2'][confident_indices].astype(str)
+
+    # Calculate percent removed
+    rem = 1 - len(P1_confident) / len(STRING_PPI)
+    print(f"    {rem * 100:.2f}% of the data was removed because it is below the confidence threshold.")
+
+    # Convert Protein IDs to Gene IDs
+    # Remove species identifier 9606
+    P1_confident = P1_confident.str[5:]
+    P2_confident = P2_confident.str[5:]
+
+    # Map protein IDs to gene IDs
+    P1_gene_id, remove1 = stable_ID_map(P1_confident.tolist(), True)
+    P2_gene_id, remove2 = stable_ID_map(P2_confident.tolist(), True)
+
+    # Remove rows that were unmapped
+    remove_rows = np.unique(np.concatenate([remove1, remove2]))
+    idxs = np.ones(len(P1_gene_id), dtype=bool)
+    idxs[remove_rows] = False
+    P1_gene_id = np.array(P1_gene_id)[idxs]
+    P2_gene_id = np.array(P2_gene_id)[idxs]
+
+    # Calculate further removal statistics
+    rem = 1 - len(P1_gene_id) / len(P1_confident)
+    print(f"    {rem * 100:.2f}% of the data was removed because it wasn't mapped to a stable gene ID.")
+
+    # Organize data as a DataFrame
+    A_list = pd.DataFrame({
+        'Protein 1': P1_gene_id,
+        'Protein 2': P2_gene_id
+    })
+
+    # Calculate total usage
+    rem = len(P1_gene_id) / len(STRING_PPI)
+    print(f"STRING Data Access Complete: {rem * 100:.2f}% of the data was used.")
+
+    return A_list, STRING_PPI
+
+def list_combine(A_lists):
+    A_list = pd.DataFrame(columns=['Protein 1', 'Protein 2'])
+    
+    # Stack the adjacency lists from each database
+    for adj_list in A_lists:
+        A_list = pd.concat([A_list, adj_list], ignore_index=True)
+    
+    return A_list
+
+def list2mat(A_list, indexTable):
+    unique_ids = indexTable['Stable ID']
+    n = len(unique_ids)
+    gene_idxs = {id_: idx for idx, id_ in enumerate(unique_ids)}
+
+    # Construct a matrix
+    A = lil_matrix((n, n), dtype=int)
+    for index, row in A_list.iterrows():
+        protein1 = row['Protein 1']
+        protein2 = row['Protein 2']
+        if protein1 in gene_idxs and protein2 in gene_idxs:
+            A[gene_idxs[protein1], gene_idxs[protein2]] = 1
+
+    # Symmetrize the matrix
+    A = A + A.T
+    A[A > 1] = 1  # Ensuring that the matrix contains only 1s and 0s after symmetrization
+
+    return A
+
+def reduceHWG(A, indexTable):
+    # Reduce to only include interacting genes
+    interacting_genes = np.where(np.sum(A, axis=0) > 0)[0]
+    A_reduced = A[interacting_genes][:, interacting_genes]
+    indexTable_reduced = indexTable.iloc[interacting_genes]
+
+    # Reduce to the largest connected component
+    G = nx.from_numpy_array(A_reduced)
+    largest_component = max(nx.connected_components(G), key=len)
+    keep = list(largest_component)
+
+    A_HWG = A_reduced[np.ix_(keep, keep)]
+    nodeTable = indexTable_reduced.iloc[keep]
+
+    return A_HWG, nodeTable
+
+def HWGMatrixDecomp(A_HWG, A_IndexTable):
+    TF_idxs = A_IndexTable['Transcription Factor'].values
+    
+    # B matrix (indexed using both the A and C tables)
+    B_HWG = A_HWG[:, TF_idxs]
+    
+    # C matrix
+    C_HWG = A_HWG[TF_idxs][:, TF_idxs]
+    C_IndexTable = A_IndexTable[TF_idxs]
+
+    return B_HWG, C_HWG, C_IndexTable
+
 ### CODES TO BUILD HARDWIRED GENOME A MATRIX ###
 def buildAdjacencyMatrix():
     """This function assembles the STRING and HuRI downloaded data into an adjacency matrix.
     See: https://github.com/Jpickard1/HardwiredGenome/blob/master/Code/data_handling/build/buildHWGobj.m
     """
+    thresh = 600
+
+    # Get adjacency lists
+    A_list_HuRI = list_HuRI()
+    print("HuRI Data Accessed")
+
+    A_list_STRING, _ = list_STRING(thresh)
+    print("STRING Data Accessed")
+
+    # Combine the lists
+    A_list = list_combine([A_list_HuRI, A_list_STRING])
+    
+    # Build the matrix
+    A_HWG = list2mat(A_list, indexTable)
+
+    # Reduce the matrix and list to only contain used genes
+    A_HWG, A_IndexTable = reduceHWG(A_HWG, indexTable)
+
+    # Construct the factor matrices
+    B_HWG, C_HWG, C_IndexTable = HWGMatrixDecomp(A_HWG, A_IndexTable)
+
+    # Package as the Hardwired Genome object
+    HWG = {
+        'thresh': thresh,
+        'A': A_HWG,
+        'B': B_HWG,
+        'C': C_HWG,
+        'geneIndexTable': A_IndexTable,
+        'TFIndexTable': C_IndexTable
+    }
+
+    return HWG
     pass
